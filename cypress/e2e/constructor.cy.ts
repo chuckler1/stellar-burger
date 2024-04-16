2d88ce5ad71f3b301b2eea1db8d3170cc5c4a9f3
Denis Kozlov <denisdesdoz@yandex.ru>
2024-04-16 19:34:44 +0300

    test

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,6 +1,5 @@
 describe('Проверяем конструктор', () => {
   const URL = 'https://norma.nomoreparties.space/api';
-  console.log(Cypress.env());
   it('Перехват запроса на получение списка ингредиентов', () => {
     cy.visit('localhost:4000');
     cy.intercept('GET', `${URL}/ingredients`, {
